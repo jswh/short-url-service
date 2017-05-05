3fdf9c82a13a90939e6d4493206c73d7e6d4eb79
jswh <jswh@jswh.me>
2017-05-05 16:24:26 +0800

    add ribbon

--- index.php
+++ index.php
@@ -66,6 +66,7 @@ if ($code) {
 </head>
 
 <body>
+<a href="https://github.com/jswh/short-url-service"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>
     <h1>Get A Short Url</h1>
     <form action="_s" method="POST">
         <input type="text" id="url" name="origin" value="" placeholder="origin url">
